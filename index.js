42bca25a5df2728f0871c8f4b6db228f6df2d670
Matthew McIvor <matthewrmcivor@gmail.com>
2024-11-11 14:16:11 -0800

    succesful api fetch request

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+const fetchData = async() => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'fec20d8a',
+      s: 'Avengers',
+      i: 'tt3896198' //unsure what this is but in url from api email
+    }
+  });
+  
+  console.log(response.data);
+};
+
+fetchData();
