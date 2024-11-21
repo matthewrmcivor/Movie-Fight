d7409fa83b510cb575e5582b3bcf3694db372b37
Matthew McIvor <matthewrmcivor@gmail.com>
2024-11-20 21:56:09 -0800

    added handling of empty array

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const fetchData = async(searchTerm) => {
     params: {
       apikey: 'fec20d8a',
       s: searchTerm
-      // i: 'tt4154756' //still not sure why search wont work without this i param (which is for a specific movie id)
     }
   });
+  
+  if (response.data.Error){
+    return [];
+  }
   
   return response.data.Search;
 };
